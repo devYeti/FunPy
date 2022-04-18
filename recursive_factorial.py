11bf31e0f8cc411f796fea4823930387f145e0f8
David Wakeley <devyeti1337@gmail.com>
2022-04-18 14:48:33 -0600

    added input to recursive _factorial

--- recursive_factorial.py
+++ recursive_factorial.py
@@ -1,5 +1,11 @@
-def factorial():
+def factorial(n):
     if n == 1:
         return 1
     else:
         return n * factorial(n - 1)
+    
+def run_fact():
+    num = int(input("Enter an integer: "))
+    factorial(num)
+    
+run_fact()
